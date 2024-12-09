c34d4e60da45a9c192bfe80acaa9ce1d89bdfe6c
Alex <licazago@fiec.espol.edu.ec>
2024-12-09 10:18:19 -0500

    STOP 5 Guia 17

---
+++ src/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDMwK7LT5DkHU9sG_Es4e3QA_y-WVwfaMc",
+    authDomain: "hibrida-250f9.firebaseapp.com",
+    projectId: "hibrida-250f9",
+    storageBucket: "hibrida-250f9.firebasestorage.app",
+    messagingSenderId: "1034898421486",
+    appId: "1:1034898421486:web:1a04406b365da5ad53e5ff"
+};  
